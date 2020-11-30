77490749fb783f1a1a021fa592d31d7681a43d5a
gaochunbo <1814593561@qq.com>
2020-11-30 20:27:44 +0800

    modify do_pack.sh

--- do_pack.sh
+++ do_pack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#±äÁ¿¶¨Òå
+#å˜é‡å®šä¹‰
 
 System="LINUX_X64"
 version="V2.0.0"
@@ -9,58 +9,56 @@ Productname="TRANSCODER"
 
 PROJECT_PATH_DIR=${PWD}
 
+OPEN_CV       = ${PROJECT_PATH_DIR}/cv
+FFMPEG        = ${PROJECT_PATH_DIR}/ffmpeg
+CODE_FFMPEG   = ${PROJECT_PATH_DIR}/code/ffmpeg-4.3.1.tar.xz
+CODE_JEMALLOC = ${PROJECT_PATH_DIR}/code/jemalloc-5.2.1.tar
+CODE_REDIS    = ${PROJECT_PATH_DIR}/code/redis-5.0.10.tar.gz
+CODE_YASM     = ${PROJECT_PATH_DIR}/code/yasm-1.3.0.tar.gz
+
 function tar_save()
 {
-	FILE_NAME=${Productname}_${System}_${version}_Build$(date +%y%m%d)
-	ZIP_NAME=${FILE_NAME}.zip 
-	echo "******start zip $ZIP_NAME***********"
-	echo ""
-	if [ -e "${FILE_NAME}.zip" ]; then
+    FILE_NAME=${Productname}_${System}_${version}_Build$(date +%y%m%d)
+    ZIP_NAME=${FILE_NAME}.zip 
+    echo "******start zip $ZIP_NAME***********"
+    echo ""
+    if [ -e "${FILE_NAME}.zip" ]; then
         rm ${FILE_NAME}.zip
     fi
-	zip -r ${ZIP_NAME} ${PROJECT_PATH_DIR}
-	#txtÎÄ¼ş´æÔÚÊ±ÏÈÉ¾³ıÎÄ¼ş
+    zip -r ${ZIP_NAME} ${PROJECT_PATH_DIR} -x ${PROJECT_PATH_DIR}/ffmpeg/3rdparty  ${PROJECT_PATH_DIR}/code/yasm-1.3.0  ${PROJECT_PATH_DIR}/code/ffmpeg-4.3.1
+    #txtæ–‡ä»¶å­˜åœ¨æ—¶å…ˆåˆ é™¤æ–‡ä»¶
     if [ -e "${FILE_NAME}.txt" ]; then
         rm ${FILE_NAME}.txt
     fi
 
     echo "dav_md5:$ATTR_LIB_MD5" >> ${FILE_NAME}.txt
-	ATTR_ZIP_MD5=`md5sum $ZIP_NAME | cut -d ' ' -f1` 
-	echo "zip_md5=$ATTR_ZIP_MD5" >> ${FILE_NAME}.txt
-	echo "type:STD">>${FILE_NAME}.txt
-    echo "lang:CN">>${FILE_NAME}.txt
-    echo "pack_ver:"${version}_$(date +%y%m%d)>>${FILE_NAME}.txt
-    echo "dav_name:"${LIBNAME}>>${FILE_NAME}.txt
-    echo "platform:"${platform%_*}>>${FILE_NAME}.txt
-    echo "plat_ver:"${platform#*_}>>${FILE_NAME}.txt
-	
-	if [ -n "${output_dir}" ]; then 
+    ATTR_ZIP_MD5=`md5sum $ZIP_NAME | cut -d ' ' -f1` 
+    echo "zip_md5=$ATTR_ZIP_MD5" >> ${FILE_NAME}.txt
+    
+    if [ -n "${output_dir}" ]; then 
         mv ${ZIP_NAME} ${output_dir}
         mv ${FILE_NAME}.txt ${output_dir}
     fi
-	echo ""
-	echo "*******end zip $ZIP_NAME*********"
-
+    echo ""
+    echo "*******end zip $ZIP_NAME*********"
 }
 
 
-function Action()
-{
-    tar_save
-}
 
-#-------------------- ½Å±¾Ö´ĞĞÈë¿Ú --------------------#
-# ½Å±¾´«²Î£º
-# 1).×Ô¶¯»¯´ò°ü£º$1Îªjenkins×Ô¶¯»¯±êÖ¾ $2Îª×Ö·û´®²ÎÊı
-# 2).±¾µØ±àÒëÊ±£º$1ÎªÖ´ĞĞ¶¯×÷ $2ÎªÓïÑÔÑ¡Ïî
+#-------------------- è„šæœ¬æ‰§è¡Œå…¥å£ --------------------#
+# è„šæœ¬ä¼ å‚ï¼š
+# 1).è‡ªåŠ¨åŒ–æ‰“åŒ…ï¼š$1ä¸ºjenkinsè‡ªåŠ¨åŒ–æ ‡å¿— $2ä¸ºå­—ç¬¦ä¸²å‚æ•°
+# 2).æœ¬åœ°ç¼–è¯‘æ—¶ï¼š$1ä¸ºæ‰§è¡ŒåŠ¨ä½œ $2ä¸ºè¯­è¨€é€‰é¡¹
 
-# ÅĞ¶ÏÊÇ×Ô¶¯»¯´ò°ü»¹ÊÇ±¾µØ´ò°ü
+# åˆ¤æ–­æ˜¯è‡ªåŠ¨åŒ–æ‰“åŒ…è¿˜æ˜¯æœ¬åœ°æ‰“åŒ…
 
+echo '"${PROJECT_PATH_DIR}/ffmpeg/3rdparty"'
 
-Action 
+tar_save
+
 if [ $? -ne 0 ]; then
-	echo "action func failed!!!"
-	return 1
+    echo "action func failed!!!"
+    return 1
 fi  
 
     
--- python/brandcompare.py
+++ python/brandcompare.py
@@ -3,13 +3,11 @@ import pandas as pd;
 df1 = pd.DataFrame(pd.read_excel('test.xlsx'))
 df2 = pd.DataFrame(pd.read_excel("group.xlsx"))
 
-print(df2)
-
 def match_group(x):
     for y in df2['key_word']:
         if str(y) in x:
             return y
-    return '未收录'
+    return ''
 
-df1['group'] = df1['add'].apply(match_group)
+df1['target'] = df1['add'].apply(match_group)
 df1.to_excel('result.xlsx',index=None)
